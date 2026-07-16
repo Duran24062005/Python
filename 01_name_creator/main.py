19b126a69c58aac2b077f25b0bb0cd196229edcc
Alexi Dg <alexisdurangomez588@gmail.com>
2026-07-15 23:11:58 -0500

    feat: :fire: day two has been added

---
+++ 01_name_creator/main.py
@@ -0,0 +1,6 @@
+# Welcome simple name creator
+# You only need to answer two question
+print(input("¿De que tú emprendimiento?: "))
+print(input("¿Cual es tu nombre?: "))
+
+print('El nombre de tu cerveza es:\n\t"'+input("De que está hecha tu cerveza: ") + input("Cual es la esecia de tú cerveza: ") +'"')
